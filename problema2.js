34b8eb72971dc091acae01d2dfe20754298e9370
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-09 21:51:00 -0500

    Haciendo la peticion al archivo data.son para obtener los datos. Selecionar los datos de todas las regiones excepto Lima y Callao y creando la lista datasets para registrar los datos para el grafico

--- problema2.js
+++ problema2.js
@@ -2,4 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     graphic();
 });
 
-function graphic() {}
+function graphic() {
+    fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+
+        var filteredRegions = data.filter(region => region.region !== "Lima" && region.region !== "Callao");
+
+        var datasets = [];
+    });
+}
